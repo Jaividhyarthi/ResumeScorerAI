bb18b13562af5aba6472b350e571e88a7f4eb039
Jaividhyarthi <141346602+Jaividhyarthi@users.noreply.github.com>
2026-05-19 10:33:38 +0530

    Update app.py

--- app.py
+++ app.py
@@ -234,4 +234,7 @@ def clear_all():
 
 if __name__ == '__main__':
     print('ResumeIQ starting on http://localhost:5000')
-    app.run(debug=True, port=5000)
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)), debug=False)
+
+
+
